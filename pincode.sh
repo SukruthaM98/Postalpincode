b3f27f350e08b92ec1a1c29d1b94ec4dcf18af8f
SukruthaM98 <msukrutha@gmail.com>
2020-10-13 21:17:16 +0530

    6 digit number for pincode validated

--- pincode.sh
+++ pincode.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -x
 
 echo "welcome to postalpincode program"
+
+read -p "enter pincode: " pincode
+
+pincodepattern='^[0-9]{6}$'
+
+if [[ $pincode =~ $pincodepattern ]]
+then
+        echo "valid"
+else
+        echo "invalid"
+fi
+
+
