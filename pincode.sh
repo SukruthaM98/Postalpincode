6901549eb7ee7f18603242fb785d55f5d57a030f
SukruthaM98 <msukrutha@gmail.com>
2020-10-13 21:31:54 +0530

    restricted code from taking alphabets at begining

--- pincode.sh
+++ pincode.sh
@@ -4,7 +4,8 @@ echo "welcome to postalpincode program"
 
 read -p "enter pincode: " pincode
 
-pincodepattern='^[0-9]{6}$'
+#should not take alphabet at the starting
+pincodepattern='^[0-9]{6}'
 
 if [[ $pincode =~ $pincodepattern ]]
 then
