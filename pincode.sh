8827532d2004ab4b9583c7db3e5966ca9cc0fe12
SukruthaM98 <msukrutha@gmail.com>
2020-10-13 21:28:29 +0530

    6 digit number validation

--- pincode.sh
+++ pincode.sh
@@ -3,8 +3,8 @@
 echo "welcome to postalpincode program"
 
 read -p "enter pincode: " pincode
-#should not take alphabet at the starting
-pincodepattern='^[0-9]{6}'
+
+pincodepattern='[0-9]{6}'
 
 if [[ $pincode =~ $pincodepattern ]]
 then
